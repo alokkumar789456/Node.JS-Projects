84cd7bdf0151df3ba8eeca9b6969965369d26e31
alok-k-idexcel <alok.k@idexcel.com>
2024-09-17 17:20:09 +0530

    jj

--- NodeJS_YT/txt.md
+++
@@ -1,3 +0,0 @@
-# PLAYLIST (NODE.JS)
-https://www.youtube.com/playlist?list=PLbtI3_MArDOkXRLxdMt1NOMtCS-84ibHH
-
--- nodejsW3/myfirst.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'})
-    res.end('hello world!')
-}).listen(3000,()=>{console.log('connected');})
-
