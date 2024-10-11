05b70e786288a196960affcc1e2787ce6776537f
alok-k-idexcel <alokkumar77954@gmail.com>
2024-10-11 19:00:12 +0530

    summary

--- Image-Gallery/routes/google.js
+++ Image-Gallery/routes/google.js
@@ -1,80 +1,95 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const session = require('express-session');
-const GoogleUser = require('../model/googleModel.js');
-require('dotenv').config();
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require("express-session");
+const GoogleUser = require("../model/googleModel.js");
+require("dotenv").config();
 
 const googleRoute = express.Router();
 
 // Configure session middleware
-googleRoute.use(session({ secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true }));
+googleRoute.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Initialize Passport
 googleRoute.use(passport.initialize());
 googleRoute.use(passport.session());
 
 // Configure Passport with Google Strategy
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.BASE_URL}/google/mainPage`
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BASE_URL}/google/mainPage`,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
         let user = await GoogleUser.findOne({ googleId: profile.id });
         if (!user) {
-            user = await GoogleUser.create({
-                username: profile.displayName,
-                email: profile.emails[0].value,
-                googleId: profile.id,
-                profilePicture: profile.photos[0].value,
-                accessToken,
-                refreshToken
-            });
+          user = await GoogleUser.create({
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+            profilePicture: profile.photos[0].value,
+            accessToken,
+            refreshToken,
+          });
         } else {
-            user.profilePicture = profile.photos[0].value;
-            await user.save();
+          user.profilePicture = profile.photos[0].value;
+          await user.save();
         }
         return done(null, user);
-    } catch (error) {
+      } catch (error) {
         return done(error, null);
+      }
     }
-}));
+  )
+);
 
 // Serialize user
 passport.serializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
 // Deserialize user
 passport.deserializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
 // Google authentication routes
-googleRoute.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+googleRoute.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-googleRoute.get('/mainPage',
-    passport.authenticate('google', { failureRedirect: '/' }),
-    (req, res) => {
-        const loggedIn = true; 
-        const user = req.user; 
-        let images = [];
+googleRoute.get(
+  "/mainPage",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const loggedIn = true;
+    const user = req.user;
+    let images = [];
 
     //    console.log(user);
     //    console.log(images);
-        if (user && Array.isArray(user.imageUrls)) {
-            images = user.imageUrls; 
-        }
-
-        // Debugging output
-        console.log('Logged in:', loggedIn);
-        console.log('User:', user);
-        console.log('Images being sent to the template:', images);
-
-        // Render mainPage with user and images data
-        res.render('mainPage', { loggedIn, user, images });
+    if (user && Array.isArray(user.imageUrls)) {
+      images = user.imageUrls;
     }
-);
+
+    // Debugging output
+    console.log("Logged in:", loggedIn);
+    console.log("User:", user);
+    console.log("Images being sent to the template:", images);
+
+    // Render mainPage with user and images data
+    res.render("mainPage", { loggedIn, user, images });
+  }
+)
 
 module.exports = googleRoute;
---
+++ prac/newOne/jsDay9999.js
@@ -0,0 +1,85 @@
+//? js - js is a language
+//? object-oriented language - anything in js considered as object 
+
+//! GFG
+
+//! types of consoles 
+
+// console.log("Hello World! I Don't Give a Bug");
+
+// console.error("This is the error statement")
+
+// console.warn("this is a warn statement")
+
+// console.table({'a':1, 'b':2});
+
+// console.time('abc')
+// setTimeout(()=>{console.log('siuuuuuuuuu!')},9000)
+// console.timeEnd('abc')
+
+// for (let i = 0; i < 5; i++) {
+//     console.count(i);
+    
+// }
+
+//! console.group() and console.groupEnd() method
+// console.group('simple');
+//   console.warn('warning!');
+//   console.error('error here');
+//   console.log('viii vina vice');
+// console.groupEnd('simple');
+// console.log('new section');
+
+
+//! Custom Console log example
+// const spacing = '10px';
+// const styles = 
+    //   `padding: ${spacing}; background-color: white; color: green; font-style: 
+    //    italic; border: 1px solid black; font-size: 2em;`;
+// console.log('%cGeeks for Geeks', styles);
+
+//! to clear console 
+// console.clear();
+
+//! es3 ECMAScript 3 (1999)
+
+//! regular expression = it is a search pattern to search a related string or to search a string 
+//! remaining google it 
+
+//! try catch block and finally (No matter what finally will always get executed)
+// try {
+//     let result = 10 / 0;
+//     console.log(result); // No error here, so this runs
+//     // throw new Error("An error occurred!"); // Now an error is thrown
+//   } catch (error) {
+//     console.log("Caught an error:"); // This runs when an error is thrown
+//     // throw new Error('if this not executed then finally gets executed')
+//   } finally {
+//     console.log("FINALLY  GOT EXECUTED");
+// }
+  
+//! Errors and expectational Handling 
+//! 1. syntax error, 2. logical error, 3. Runtime error 
+
+
+//! Switch Statements 
+// let grade = 'A'
+// let result;
+
+// switch (grade) {
+//     case 'A':
+//         result = "Grade is excellent"
+//         break;
+//     case 'B':
+//         result = "Grade is good"
+//         break;
+//     case 'C':
+//         result = "Grade is Average "
+//         break;
+//     case 'D':
+//         result = "Grade is Poor"
+//         break;
+//     default:
+//         text = "NO grades achieved";
+// }
+// console.log(result)
