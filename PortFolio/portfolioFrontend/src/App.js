2957841305d4d0861c3c83be35331ba56877cdc9
alok-k-idexcel <alokkumar77954@gmail.com>
2024-10-16 17:59:30 +0530

    portfolio

--- PortFolio/portfolioFrontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-700">
-      Hello world! 
-    </h1>
-  )
-}
--- prac/newOne/jsDay9999.js
+++ prac/newOne/jsDay9999.js
@@ -1,9 +1,71 @@
 //? js - js is a language
-//? object-oriented language - anything in js considered as object 
+//? object-oriented language - anything in js considered as object
 
+//! References 
 //! GFG
+//! https://www.javascripttutorial.net/
 
-//! types of consoles 
+
+//! Data Types
+//? primitive (Basic form of data type )
+//? immutable, meaning that they cannot be modified once created.
+//? null, undefined, string, boolean, number, symbol, bigint
+//? Null == Undefined
+
+// const a = null;
+// console.log("a: ", typeof(a)); // null
+
+// const b = "b";
+// console.log("b: ", typeof b); // String
+
+// const c = true;
+// console.log("c: ", typeof(c)); // boolean
+
+// const d = 3;
+// console.log("d: ", typeof d); // Number
+// //? NAN is a number (numeric value)
+//! types of Number https://www.javascripttutorial.net/javascript-number/
+
+// console.log("a" / 2, "is a number it came when we divided it by a by 2"); // Nan
+// const e = Symbol('description1'); // Symbols , is unique Char
+// const e1 = Symbol('description1'); // This is a different symbol, even though the description is the same
+// console.log( "e:", typeof(e), "&& e1:", typeof(e1) ,e === e1 , "when checked that they are equal"); // Output: false
+
+// // BigInt larger than 2^53 – 1
+// const f = 7243687125617n; // n is written at last to say that its bigInt to JS
+// console.log("f: ",typeof(f));
+
+
+//! var let Const
+//? var can be initialized, declared, reinitialized, redeclared
+// var a;
+// var a = 'b'
+// a = 3
+
+//? let cannot be redeclared can be initialized, declared, reinitialized
+// let a = 'b'
+// a = 3
+
+//? const cannot be redeclared, reinitialized can be initialized and declared but at the same time
+// const a = "b"
+
+//! statements
+// console.log("this is a statement");
+
+//! Scopes 
+//! block // anything inside the the Curly braces 
+// block = true
+// if(block){
+//     console.log("anything inside");
+//     console.log("curly braces");
+//     console.log("are part of Block");
+// }
+
+//! variable scopes 
+//! global scope // which is accessible anywhere 
+//! Block // Which is accessible only inside block 
+
+//! types of consoles
 
 // console.log("Hello World! I Don't Give a Bug");
 
@@ -19,7 +81,7 @@
 
 // for (let i = 0; i < 5; i++) {
 //     console.count(i);
-    
+
 // }
 
 //! console.group() and console.groupEnd() method
@@ -30,56 +92,47 @@
 // console.groupEnd('simple');
 // console.log('new section');
 
-
 //! Custom Console log example
 // const spacing = '10px';
-// const styles = 
-    //   `padding: ${spacing}; background-color: white; color: green; font-style: 
-    //    italic; border: 1px solid black; font-size: 2em;`;
+// const styles =
+//   `padding: ${spacing}; background-color: white; color: green; font-style:
+//    italic; border: 1px solid black; font-size: 2em;`;
 // console.log('%cGeeks for Geeks', styles);
 
-//! to clear console 
+//! to clear console
 // console.clear();
 
-//! es3 ECMAScript 3 (1999)
+//! Types of JavaScript Operators
+//! w3 school 
+//! Arithmetic Operators
+// Operator	    Description
+//    +	        Addition
+//    -	        Subtraction
+//    *	        Multiplication
+//    **	    Exponentiation (ES2016)
+//    /	        Division
+//    %	        Modulus (Division Remainder)
+//    ++	    Increment
+//    --	    Decrement
 
-//! regular expression = it is a search pattern to search a related string or to search a string 
-//! remaining google it 
+//! Assignment Operators
+// Operator	   Example	    Same As
+//    =	       x = y	    x = y
+//    +=	   x += y	    x = x + y
+//    -=	   x -= y	    x = x - y
+//    *=	   x *= y	    x = x * y
+//    /=	   x /= y	    x = x / y
+//    %=	   x %= y	    x = x % y
+//    **=	   x **= y	    x = x ** y
 
-//! try catch block and finally (No matter what finally will always get executed)
-// try {
-//     let result = 10 / 0;
-//     console.log(result); // No error here, so this runs
-//     // throw new Error("An error occurred!"); // Now an error is thrown
-//   } catch (error) {
-//     console.log("Caught an error:"); // This runs when an error is thrown
-//     // throw new Error('if this not executed then finally gets executed')
-//   } finally {
-//     console.log("FINALLY  GOT EXECUTED");
-// }
-  
-//! Errors and expectational Handling 
-//! 1. syntax error, 2. logical error, 3. Runtime error 
-
-
-//! Switch Statements 
-// let grade = 'A'
-// let result;
-
-// switch (grade) {
-//     case 'A':
-//         result = "Grade is excellent"
-//         break;
-//     case 'B':
-//         result = "Grade is good"
-//         break;
-//     case 'C':
-//         result = "Grade is Average "
-//         break;
-//     case 'D':
-//         result = "Grade is Poor"
-//         break;
-//     default:
-//         text = "NO grades achieved";
-// }
-// console.log(result)
+//! Comparison Operators
+// Operator	 Description
+//    ==	 equal to
+//    ===	 equal value and equal type
+//    !=	 not equal
+//    !==	 not equal value or not equal type
+//    >	     greater than
+//    <	     less than
+//    >=	 greater than or equal to
+//    <=	 less than or equal to
+//    ?	     ternary operator                                                                                                             
